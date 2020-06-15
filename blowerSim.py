0c34edd1bed58135a200afe17aa1e2a1bb1fd4cf
John Roper <johnroper100@gmail.com>
2020-06-15 18:06:07 -0400

    keep test code
    Signed-off-by: John Roper <johnroper100@gmail.com>

--- blowerSim.py
+++ blowerSim.py
@@ -4,6 +4,7 @@ air = 0.0
 pull = 0.0
 jitter = 50
 heaveDiff = 0.0
+index2 = 0
 for i in range(150):
     if (pull * 1.5) > heaveDiff:
         heaveDiff = pull * 1.5
@@ -15,6 +16,13 @@ for i in range(150):
     if air < 0.0:
         air = 0.0
     index += 1
+    index2 += 1
+    if index2 == 50:
+        pull = 0.1
+    if index2 == 95:
+        pull = 0.0
+        index2 = 0
     if index == jitter:
         heaveDiff = pull * 1.5
         index = 0
+    print(air)
